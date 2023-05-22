2e0d95642518738e5d363e7a07bea5eda29efc5a
emelymack <emelymack@gmail.com>
2023-05-22 19:18:13 -0300

    Updated Form validation

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -63,19 +63,19 @@ export const ContactForm = () => {
       <Form className={style.form} ref={form} onSubmit={sendEmail}>
         <Form.Group className="mb-3" controlId="from_name">
           <Form.Label className={style.label}>{(lang === 'eng') ? 'Full name / Company' : 'Nombre completo / Empresa'}</Form.Label>
-          <Form.Control type="text" placeholder={(lang === 'eng') ? 'Full name / Company' : 'Nombre completo / Empresa'} name="from_name" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> handleInputChange(e)} className={`${style.input}`}/>
+          <Form.Control type="text" placeholder={(lang === 'eng') ? 'Full name / Company' : 'Nombre completo / Empresa'} name="from_name" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> handleInputChange(e)} className={`${style.input}`} required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="from_email">
           <Form.Label className={style.label}>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email" name="from_email" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> handleInputChange(e)} className={`${style.input}`} />
+          <Form.Control type="email" placeholder="Email" name="from_email" onChange={(e:React.ChangeEvent<HTMLInputElement>)=> handleInputChange(e)} className={`${style.input}`} required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="message">
           <Form.Label className={style.label}>{(lang === 'eng') ? 'Message' : 'Mensaje'}</Form.Label>
-          <Form.Control as="textarea" aria-label="Message" name="message" placeholder={(lang === 'eng') ? 'Your proposal, feedback, comments... everything is appreciated :)' : 'Tu propuesta, feedback, comentarios... todo se aprecia :)'} rows={5} onChange={(e:React.ChangeEvent<HTMLInputElement>)=> handleInputChange(e)} className={`${style.input} px-3 py-2`}/>
+          <Form.Control as="textarea" aria-label="Message" name="message" placeholder={(lang === 'eng') ? 'Your proposal, feedback, comments... everything is appreciated :)' : 'Tu propuesta, feedback, comentarios... todo se aprecia :)'} rows={5} onChange={(e:React.ChangeEvent<HTMLInputElement>)=> handleInputChange(e)} className={`${style.input} px-3 py-2`} required />
         </Form.Group>
         <div className='d-flex justify-content-center justify-content-lg-end'>
-        <Button className='btn-secondary px-5 py-2' type="submit">
-        {(lang === 'eng') ? 'Submit' : 'Enviar'}
+          <Button className='btn-secondary px-5 py-2' type="submit">
+          {(lang === 'eng') ? 'Submit' : 'Enviar'}
         </Button>
         </div>
       </Form>
